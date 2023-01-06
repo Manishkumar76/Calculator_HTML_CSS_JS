6ce376535f6e23135fd60819419fab7a0d0ff96b
MANISH KUMAR <117758302+Manishkumar76@users.noreply.github.com>
2023-01-06 18:45:41 +0000

    calculator

--- script.js
+++ script.js
@@ -23,6 +23,13 @@ Array.from(buttons).forEach((button) => {
 
 
     }
+    else if (e.target.innerHTML == '%') {
+
+      string = eval(string / 100);
+      document.querySelector('input').value = string;
+
+
+    }
     else {
       console.log(e.target)
       string = string + e.target.innerHTML;
